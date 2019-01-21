7b4b4bc1a5562de9a8a66b1631875b231308602b
proepkes <phil.boerner@fh-muenster.de>
2019-01-21 16:20:51 +0100

    Server console-input

--- Engine/Server/Program.cs
+++ Engine/Server/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;      
 
 namespace Server
 {
     class Program
-    {                                 
-        private const int RoomSize = 2; 
-
+    {                                   
         static void Main(string[] args)
-        {                    
+        {
+            int roomSize = 2;
+            int waitInSeconds = 5;
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Console.Write($"Enter room size (defaults to {roomSize} after {waitInSeconds} seconds): ");
+            while (!Console.KeyAvailable && sw.Elapsed.Seconds < waitInSeconds)
+            {                  
+                Thread.Sleep(1);
+            }
+            sw.Stop();
+
+            if (Console.KeyAvailable)
+            {
+                roomSize = Console.ReadKey().KeyChar - 48;             
+            }
+            Console.Write(Environment.NewLine);
+
             var room = new Room();
 
-            room.Open(RoomSize);         
+            room.Open(roomSize);         
 
             while (!Console.KeyAvailable)
             {
