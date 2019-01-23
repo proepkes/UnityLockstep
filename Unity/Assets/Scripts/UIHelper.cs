07a7b1f96e9aad5103c450051751eb9b3d2c3d1d
proepkes <phil.boerner@fh-muenster.de>
2019-01-23 17:00:53 +0100

    Remove TMP

--- Unity/Assets/Scripts/UIHelper.cs
+++ Unity/Assets/Scripts/UIHelper.cs
@@ -1,23 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHelper : MonoBehaviour
 {
-    public TMP_Text AgentCountText;
-    public TMP_Text ConnectedText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text ConnectedText;
+    public Text AgentCountText;    
 
     // Update is called once per frame
     void Update()
     {
-        AgentCountText.text = "Agents: " + Contexts.sharedInstance.game.GetEntities().Length;
         ConnectedText.text = "Connected: " + LockstepNetwork.Instance.Connected;
+        AgentCountText.text = "Agents: " + Contexts.sharedInstance.game.GetEntities().Length;
     }
 }
