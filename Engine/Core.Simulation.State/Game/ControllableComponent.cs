18fabf926ead6957f038a6112bf63906c65f18e7
proepkes <phil.boerner@fh-muenster.de>
2019-02-09 23:55:09 +0100

    Remove unreferenced projects

--- Engine/Core.Simulation.State/Game/ControllableComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Entitas;
-
-namespace Lockstep.Core.Components.Game
-{
-    [Game]
-    public class ControllableComponent : IComponent
-    {
-    }
-}
--- Engine/Core.Simulation.State/Jenny-Gen.bat
+++
@@ -1,1 +0,0 @@
-..\Jenny\Jenny.exe gen
