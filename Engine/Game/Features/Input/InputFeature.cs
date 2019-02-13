a0714ac25c36524ff59732d9ce4883606de5677f
proepkes <phil.boerner@fh-muenster.de>
2019-02-13 17:45:43 +0100

    performance++

--- Engine/Game/Features/Input/InputFeature.cs
+++ Engine/Game/Features/Input/InputFeature.cs
@@ -6,7 +6,10 @@
         {
             //TODO: Add InputValidationSystem  
             Add(new ExecuteSpawnInput(contexts, services));
-            Add(new VerifySelectionIdExists(contexts));
+
+            //Massive performance-hit:
+            //Add(new VerifySelectionIdExists(contexts));
+
             Add(new ExecuteNavigationInput(contexts, services));
             //TODO: Add CleanupInput that removes input of validated frames (no rollback required => can be removed)
         }
